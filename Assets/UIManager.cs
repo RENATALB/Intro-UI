a2fa98fe534ccdd20e53f42b8072e627bb40f8df
RENATALB <166546709+RENATALB@users.noreply.github.com>
2025-04-21 12:04:57 -0300

    no anda

---
+++ Assets/UIManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI TxtSaludo;
+    public TMP_InputField InputNombre;
+    string nombreIngresado;
+    string saludo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TxtSaludo.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Saludar()
+    {
+        // Obtener el nombre del inputField
+
+        nombreIngresado = InputNombre.text;
+
+        // Concatenar el nombre con un saludo
+
+        saludo = "¡Hola " + nombreIngresado + "!";
+
+        // Mostrar el saludo en txtSaludo
+
+        TxtSaludo.text = saludo;
+    }
+}
